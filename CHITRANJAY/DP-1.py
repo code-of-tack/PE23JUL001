ced7c0af3cf3ce074d61376c03e6806fc768b82d
Chitranjay Singh Choudhari <130092897+Chitranjay@users.noreply.github.com>
2023-08-09 00:49:49 +0530

    Update DP-1.py

--- CHITRANJAY/DP-1.py
+++ CHITRANJAY/DP-1.py
@@ -1,3 +1,12 @@
+"""
+You are part of a team developing a financial application called "FinRevise."
+As part of this application, you have been assigned the task of creating a feature that allows users to reverse a given number.
+This feature will assist users in performing financial calculations, checking numeric data integrity,
+and managing their financial transactions effectively.
+To accomplish this, you decide to create a Python program that takes a number as input and reverses it.
+The program will provide users with the reversed number, along with additional functionality to enhance their financial management capabilities.
+"""
+
 def calculate_sum_and_average(numbers):
     # Convert the space-separated input string into a list of float numbers
     numbers = [float(i) for i in numbers.split()]
@@ -8,8 +17,7 @@ def calculate_sum_and_average(numbers):
 
     return total_sum, average
 
-
-if __name__ == "__main__":
+def main():
     print("Welcome to QuantFinTech Data Analytics!")
     numbers_input = input("Please enter a series of numerical data points separated by spaces: ")
 
@@ -17,3 +25,6 @@ if __name__ == "__main__":
     print("\nThank you for providing the data points.")
     print(f"The sum of the numbers is: {sum_result:.2f}")
     print(f"The average of the numbers is: {average_result:.2f}")
+
+if __name__ == "__main__":
+    main()
