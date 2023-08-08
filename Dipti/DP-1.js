3bb3ea005c914a41d1cb1fce42bca5bf5aca9d75
Dipti <diptisoni9929@gmail.com>
2023-08-08 19:30:12 +0530

    Add solution for exercise 1

--- Dipti/DP-1.js
+++ Dipti/DP-1.js
@@ -1,41 +1,27 @@
-// // calculate the sum and average 
-function calculateSumAndAverage(dataPoints) {
-    // Split the input string into an array of numbers
-    const numbers = dataPoints.split(' ').map(Number);
-  
-    // Calculate the sum of the numbers
-    const sum = numbers.reduce((acc, num) => acc + num, 0);
-  
-    // Calculate the average of the numbers
-    const average = sum / numbers.length;
-  
-    // Return an object containing the sum and average
-    return {
-      sum,
-      average
-    };
-  }
-  
-  // Function to prompt the user for input and display the results
-  const prompt = require("prompt-sync")({sigint: true});
-  function processDataPoints() {
-    const dataPoints = prompt('Please enter a series of numerical data points separated by spaces:');
-    
-    // Check if input is provided
-    if (dataPoints.trim() === '') {
-      console.log('No data points provided.');
-      return;
-    }
-    
-    // Calculate the sum and average
-    const result = calculateSumAndAverage(dataPoints);
-    
-    // Display the results
-    console.log('Thank you for providing the data points.');
-    console.log('The sum of the numbers is:', result.sum);
-    console.log('The average of the numbers is:', result.average.toFixed(2));
-  }
-  
-  // Call the function to start the program
-  processDataPoints();
+// Function to calculate the sum of an array of numbers
+function calculateSum(data) {
+  return data.reduce((acc, num) => acc + num, 0);
+}
+
+// Function to calculate the average of an array of numbers
+function calculateAverage(data) {
+  const sum = calculateSum(data);
+  return sum / data.length;
+}
+
+// Function to process user input and display the results
+function processData(input) {
+  const dataPoints = input.split(" ").map(Number);
+  const sum = calculateSum(dataPoints);
+  const average = calculateAverage(dataPoints);
+
+  console.log("Thank you for providing the data points.");
+  console.log("The sum of the numbers is:", sum.toFixed(2));
+  console.log("The average of the numbers is:", average.toFixed(2));
+}
+
+// Example usage:
+const userInput = prompt("Welcome to QuantFinTech Data Analytics!\nPlease enter a series of numerical data points separated by spaces:");
+processData(userInput);
+
 
